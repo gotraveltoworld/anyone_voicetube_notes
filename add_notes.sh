344b1aa31152c53fd26e03f1150236f9d135fe10
undefined <gotraveltoworld@gmail.com>
2018-06-21 10:29:23 +0800

    Add the tool for create the notes.

---
+++ add_notes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DAYS=30
+FOLDER='other_data'
+# For example: vt20171130
+for (( i=0; i<=$DAYS; i=i+1 ))
+do
+    now=$(date -v +${i}d '+%Y%m%d')
+    if test -e $FOLDER/vt$now.md; then
+        echo $now', Exist'
+    else
+        echo $now', No Exist'
+        # cp notes/note_paradigm.md $FOLDER/vt$now.md
+        touch $FOLDER/vt$now.md
+    fi
+done
